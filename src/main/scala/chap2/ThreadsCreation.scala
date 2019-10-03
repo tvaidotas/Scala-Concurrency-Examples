b419b98e4184c574203b1b6a124c108913013916
tvaidotas <tadas.vaidotas@gmail.com>
2019-10-03 18:10:18 +0100

    thread execution example a thread method to execute block of code on a new thread

---
+++ src/main/scala/chap2/ThreadsCreation.scala
@@ -0,0 +1,26 @@
+package chap2
+
+object ThreadsCreation extends App {
+
+  class MyThread extends Thread {
+    override def run(): Unit = {
+      println("New thread running")
+    }
+  }
+
+  val thread: Thread = new MyThread()
+  thread.start()
+  thread.join()
+  println("new thread joined")
+
+  def thread(body: => Unit): Thread ={
+    val thread = new Thread{
+      override def run() = body
+    }
+    thread.start()
+    thread
+  }
+
+
+
+}
